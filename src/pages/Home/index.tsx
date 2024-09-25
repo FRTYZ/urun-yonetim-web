e5a4ed7102e9cdaa0f797247a520626e7cbcee2b
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-09-25 04:58:13 +0300

    Edited home page for create product form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,9 +7,17 @@ import { Request } from '../../helpers/Request';
 // Components
 import DataTable from '../../components/DataTable';
 import TableLazy from '../../components/TableLazy';
+import Drawer from '../../components/Drawer';
 
-// React Query
+// Form Elements
+import XButton from '../../components/FormElements/XButton';
+import XInput from '../../components/FormElements/XInput';
+import XFile from '../../components/FormElements/XFile';
+
+// Other npm packages
 import { useQuery } from 'react-query';
+import { useFormik } from 'formik';
+import Swal from 'sweetalert2'
 
 // Interfaces
 export interface ProductsProps {
@@ -63,7 +71,7 @@ function index() {
                 accessorKey: "_id",
                 header: ({ column }) => {
                     return (
-                        <div className="flex items-center gap-2 cursor-pointer hover:text-zinc-700">
+                        <div  className="flex items-center gap-2 cursor-pointer hover:text-zinc-700">
                             Product Image
                         </div>
                     );
@@ -74,14 +82,14 @@ function index() {
                     return thumbnail ? (
                         <img
                             alt="Product image"
-                            className="aspect-square rounded-md object-cover mr-4"
+                             className="aspect-square rounded-md object-cover mr-4"
                             height="64"
                             src={thumbnail}
                             width="64"
                         />
                     ) : (
-                        <div className="size-16 rounded-md bg-slate-300 flex justify-center items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera text-slate-100" viewBox="0 0 16 16">
+                        <div  className="size-16 rounded-md bg-slate-300 flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  className="bi bi-camera text-slate-100" viewBox="0 0 16 16">
                             <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z"/>
                             <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
                         </svg>
@@ -94,7 +102,7 @@ function index() {
                 header: ({ column }) => {
                     return (
                         <div
-                            className="flex items-center gap-2 whitespace-nowrap cursor-pointer hover:text-zinc-700"
+                             className="flex items-center gap-2 whitespace-nowrap cursor-pointer hover:text-zinc-700"
                             onClick={() =>
                                 column.toggleSorting(
                                     column.getIsSorted() === "asc"
@@ -102,7 +110,7 @@ function index() {
                             }
                         >
                             Name
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down w-4 h-4" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  className="bi bi-arrow-down w-4 h-4" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
                             </svg>
                         </div>
@@ -118,7 +126,7 @@ function index() {
                 header: ({ column }) => {
                     return (
                         <div
-                            className="flex items-center gap-2 whitespace-nowrap cursor-pointer hover:text-zinc-700"
+                             className="flex items-center gap-2 whitespace-nowrap cursor-pointer hover:text-zinc-700"
                             onClick={() =>
                                 column.toggleSorting(
                                     column.getIsSorted() === "asc"
@@ -126,7 +134,7 @@ function index() {
                             }
                         >
                             Ürün Özeti
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down w-4 h-4" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  className="bi bi-arrow-down w-4 h-4" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
                             </svg>
                         </div>
@@ -138,7 +146,7 @@ function index() {
                 header: ({ column }) => {
                     return (
                         <div
-                            className="flex items-center gap-2 cursor-pointer whitespace-nowrap hover:text-zinc-700"
+                             className="flex items-center gap-2 cursor-pointer whitespace-nowrap hover:text-zinc-700"
                             onClick={() =>
                                 column.toggleSorting(
                                     column.getIsSorted() === "asc"
@@ -146,7 +154,7 @@ function index() {
                             }
                         >
                             Price
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down w-4 h-4" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  className="bi bi-arrow-down w-4 h-4" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
                             </svg>
                         </div>
@@ -158,7 +166,7 @@ function index() {
             header: ({ column }) => {
                 return (
                     <div
-                        className="flex items-center gap-2 cursor-pointer whitespace-nowrap hover:text-zinc-700"
+                         className="flex items-center gap-2 cursor-pointer whitespace-nowrap hover:text-zinc-700"
                         onClick={() =>
                             column.toggleSorting(
                                 column.getIsSorted() === "asc"
@@ -166,7 +174,7 @@ function index() {
                         }
                     >
                         Stock
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down w-4 h-4" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  className="bi bi-arrow-down w-4 h-4" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
                         </svg>
                     </div>
@@ -175,14 +183,14 @@ function index() {
             },
             {
                 id: "actions",
-                header: () => <span className="sr-only">Actions</span>,
+                header: () => <span  className="sr-only">Actions</span>,
                 cell: ({ row }) => {
                     return (
-                        <div className="flex items-center justify-end gap-4">
+                        <div  className="flex items-center justify-end gap-4">
                             <button
-                                className="items-center gap-2"
+                                 className="items-center gap-2"
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square w-4 h-4" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  className="bi bi-pencil-square w-4 h-4" viewBox="0 0 16 16">
                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
                                 </svg>
@@ -190,9 +198,9 @@ function index() {
                             </button>
 
                             <button
-                                className="items-center gap-2"
+                                 className="items-center gap-2"
                             >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3 w-4 h-4" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  className="bi bi-trash3 w-4 h-4" viewBox="0 0 16 16">
                                 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                                 </svg>
                                 Remove
@@ -205,22 +213,186 @@ function index() {
         []
     );
 
-  return (
-    <div>
-        {products && products.length > 0 ? (
-            <DataTable
-                data={products!}
-                columns={columns}
-                header="Ürünler"
-                subHeader="Ürünlerinizi yönetin ve satış performanslarını görüntüleyin."
-                className="px-0"
-                searchFilter={true}
-            />
-        ): (
-            <TableLazy />
-        )}
-    </div>
-  )
+    // CRUD Functions
+
+    const addFormik = useFormik({
+        enableReinitialize: true,
+        initialValues: {
+            name: '',
+            description: '',
+            price: '',
+            stock: '',
+            featuredImage: []
+        },
+        onSubmit: async (values) => {
+            const {name, description, price, stock, featuredImage } = values;
+            console.log(values)
+            if(name == '' || description == '' || price == '' || stock == ''){
+             
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Hata',
+                    text: 'Gerekli alanları doldurmalısınız.',
+                })
+            }
+            else{
+                const formdata: FormData = new FormData();
+                formdata.append("name", name!);
+                formdata.append("description", description!);
+                formdata.append("price", price!);
+                formdata.append("stock", stock!);
+                formdata.append('featuredImage', featuredImage);
+                
+                const url = '/product/list';
+
+                const response = await Request({
+                    method: 'POST',
+                    url: url,
+                    formData: formdata
+                });
+                
+                const responseCheck = Object.keys(response).filter(item => item == 'success')
+                if (responseCheck) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'İşlem Başarılı',
+                        html: 'Ürün başarıyla eklend',
+                        confirmButtonText: 'Tamam'
+                    })
+                }
+                else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Hata',
+                        text: 'Bir sorun oluştu',
+                    })
+                } 
+            }
+        }
+    })
+
+    // htmlForms
+    const addhtmlForm = (
+        <div className="bg-white text-left px-4 md:p-8 mb-6">
+            <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+                <div className="text-gray-600 mb-4">
+                    <p className="font-medium text-lg">Yeni Ürün Ekleme</p>
+                    <p>Lütfen gerekli alanları doldurun</p>
+                </div>
+              
+                <form
+                    method='POST'
+                    onSubmit={addFormik.handleSubmit}
+                    encType='multipart/form-data'
+                    className="lg:col-span-2 block space-y-4"
+                >
+                    <XInput
+                        type='text'
+                        name='name'
+                        placeholder='Ürün Adı'
+                        labelType='top'
+                        label='Ürün Adı'
+                        errorMessage={Boolean(addFormik.values.name == '' && addFormik.touched.name) ? 'Ürün adı doldurmalısınız' : ''}
+                        value={addFormik.values.name}
+                        onChange={addFormik.handleChange}
+                        tabIndex={1}
+                    />
+                    <XInput
+                        type='text'
+                        name='description'
+                        placeholder='Ürün Açıklaması'
+                        labelType='top'
+                        label='Ürün Açıklaması'
+                        errorMessage={Boolean(addFormik.values.description == '' && addFormik.touched.description) ? 'Ürün açıklamasını doldurmalısınız' : ''}
+                        value={addFormik.values.description}
+                        onChange={addFormik.handleChange}
+                        tabIndex={2}
+                    />
+                    <XInput
+                        type='number'
+                        name='price'
+                        placeholder='Ürün Fiyatı'
+                        labelType='top'
+                        label='Ürün Fiyatı'
+                        errorMessage={Boolean(addFormik.values.price == '' && addFormik.touched.price) ? 'Ürün fiyatı belirlemelisiniz' : ''}
+                        value={addFormik.values.price}
+                        onChange={addFormik.handleChange}
+                        tabIndex={3}
+                    />
+                    <XInput
+                        type='number'
+                        name='stock'
+                        placeholder='Stok durumu'
+                        labelType='top'
+                        label='Stok durumu'
+                        errorMessage={Boolean(addFormik.values.stock == '' && addFormik.touched.stock) ? 'Ürün stok durumunu belirlemelisiniz' : ''}
+                        value={addFormik.values.stock}
+                        onChange={addFormik.handleChange}
+                        tabIndex={4}
+                    />
+                     <XFile 
+                        label='Ürün fotoğrafı'
+                        name="featuredImage"
+                        oldFileName=''
+                        type='image'
+                        handleFormik={addFormik}
+                        accept='image/png, image/jpeg'
+                    />
+                    <div className="md:col-span-5 text-right">
+                        <XButton 
+                            label="Ürünü Ekle"
+                            backgroundColor='bg-black'
+                            textStyle='text-white text-[16px] font-[600]'
+                            padding='px-8 py-3'
+                            radius='rounded-lg'
+                            addStyle="!w-fit"
+                            tabIndex={6}
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+
+    return (
+        <>
+            {products && products.length > 0 ? (
+                <div  className="w-full mx-auto bg-white rounded-sm border border-gray-200">
+                    <header  className="grid grid-cols-2 px-3 py-4 items-center border-b border-gray-100">
+                        <h2  className="font-semibold text-gray-800">Ürünler</h2>
+                        <div  className="text-end">
+                             <Drawer
+                                buttonContent={
+                                    <XButton 
+                                        label="Yeni Ürün Ekle"
+                                        backgroundColor='bg-gray-400'
+                                        textStyle='text-black text-[16px] font-[600]'
+                                        padding='px-4 py-1'
+                                        radius='rounded-lg'
+                                        addStyle="!w-fit"
+                                    />
+                                }
+                                backgroundColor='bg-white dark:bg-primary-dark'
+                                side='right'
+                                padding='px-8 pt-12'
+                                width='w-[80vw]'
+                            >
+                                {addhtmlForm}
+                            </Drawer>
+                        </div>
+                    </header>
+                    <DataTable
+                        data={products}
+                        columns={columns}
+                        subHeader="Ürünlerinizi yönetin ve satış perhtmlFormanslarını görüntüleyin."
+                        searchFilter={true}
+                    />
+                </div>
+            ): (
+                <TableLazy />
+            )}
+        </>
+    )
 }
 
 export default index
